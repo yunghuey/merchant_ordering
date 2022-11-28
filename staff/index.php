59475769b3021b7450fe4ba75590ebc12124dec3
Tong Yung Huey <yunghuey2002@gmail.com>
2022-11-28 14:27:55 +0800

    include leftmenu into dashboard

--- staff/index.php
+++ staff/index.php
@@ -4,13 +4,26 @@
     */
 session_start();
 
-if (isset($_SESSION['username'])){
-    echo "<h1> Welcome ".$_SESSION['username']."</h1>";
-    
-} else{
-    echo "<p> nothing here</p>";
-}
-echo "<a href='../auth/logout.php'>Logout</a>";
-
-
+if (isset($_SESSION['username'])){}
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+
+    <!-- font awesome v5 for leftmenu-->
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
+
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="leftmenu.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php include("leftmenu.php"); ?>
+
+    <!-- bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
