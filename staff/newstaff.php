58a142eedc8da1ebccf77abbe4040a2797fd3d32
Tong Yung Huey <yunghuey2002@gmail.com>
2022-11-29 22:51:16 +0800

    rename style

--- staff/newstaff.php
+++ staff/newstaff.php
@@ -18,7 +18,7 @@
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="leftmenu.css" rel="stylesheet">
-    <link href="newstaff_style.css" rel="stylesheet">
+    <link rel="stylesheet" href="template_style.css">
 
   </head>
   <body>
