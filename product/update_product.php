57fcc46fdb84fbd04ea14f6d4f066468e8fa6eff
YUNG HUEY TONG <yunghuey2002@gmail.com>
2022-12-16 12:26:14 +0800

    add function to delete old picture

--- product/update_product.php
+++ product/update_product.php
@@ -6,7 +6,16 @@
 
     $productName = $productPrice = $productCurrentQty = 
     $productCat = $productDesc = $new_img_name = $product_img_name = 
-    $img_upload_path = "";
+    $img_upload_path = $picture_name = "";
+
+    function delete_ori_prod_pic($conn,$id){
+        $picture_name_sql = "SELECT productPicture FROM product WHERE productID = ".$id;
+        $rssp = mysqli_query($conn,$picture_name_sql);
+        $rwsp = mysqli_fetch_assoc($rssp);
+        $picture_name = "product_images/".$rwsp['productPicture'];
+        unlink($picture_name); 
+    }
+
     if ($_SERVER['REQUEST_METHOD'] === "POST"){
         if(isset($_POST['updateproduct'])){
             $productName = trim($_POST['productName']);
@@ -15,12 +24,14 @@
             $productCat = trim($_POST['productCat']);
             $productDesc = trim($_POST['productDesc']);
             $id =  $_POST['id'];
+            $condition = "WHERE productID = ".$id;
 
             $update_product = "UPDATE product SET productName = '".$productName."',productPrice = '"
                                 .$productPrice."', productCurrentQty = '".$productCurrentQty."',productDesc = '"
                                 .$productDesc."',productCategory = '".$productCat."'";
             
             if (isset($_FILES['prod_img'])){
+                delete_ori_prod_pic($conn,$id);
                 $product_img_name = $_FILES['prod_img']['name'];
                 $tmp_name = $_FILES['prod_img']['tmp_name'];
                 $img_ex = pathinfo($product_img_name, PATHINFO_EXTENSION);
@@ -30,23 +41,21 @@
                 move_uploaded_file($tmp_name, $img_upload_path);
                 $update_product .= ",productPicture = '".$new_img_name."'";
             }
-            $update_product .= "WHERE productID = ".$id;
+            $update_product .= $condition;
+
             if(mysqli_query($conn,$update_product)){
-                $_SESSION['message'] = "Product ".$productName." is updated successfully";
+                $_SESSION['product'] = "Product ".$productName." is updated successfully";
                 header("location:productlist.php");
             }
         }
 
         if(isset($_POST['deleteproduct'])){
             $id = $_POST['delete_id'];
+            delete_ori_prod_pic($conn,$id);
             $delete_sql = "DELETE FROM product WHERE productID = ".$id;
             if (mysqli_query($conn,$delete_sql)){
-                $_SESSION['message'] = "Product is deleted";
                 header("location:productlist.php");
-            } else{
-                echo "<script> alert('Error occur'); </script>";
             }
         }
     }
-
 ?>
